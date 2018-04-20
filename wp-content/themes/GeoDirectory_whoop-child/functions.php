4d2d17bc0f60324d324edfe09bceb7001e650b6e
pakkawat <pakkawat.jong@gmail.com>
2018-04-20 14:34:57 +0700

    update

--- wp-content/themes/GeoDirectory_whoop-child/functions.php
+++ wp-content/themes/GeoDirectory_whoop-child/functions.php
@@ -13,11 +13,11 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( 'bootstrap-theme' ),
-        wp_get_theme()->get('Version')
-    );
+    // wp_enqueue_style( 'child-style',
+    //     get_stylesheet_directory_uri() . '/style.css',
+    //     array( 'bootstrap-theme' ),
+    //     wp_get_theme()->get('Version')
+    // );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
